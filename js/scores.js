839dcac22791b2fdf2f19d3a64722f754ebc27a0
Balqees <35161913+balqeesAldabaybah@users.noreply.github.com>
2021-09-18 15:49:11 +0300

    Add files via upload

---
+++ js/scores.js
@@ -0,0 +1,7 @@
+// Actually calling the getScores() function, 
+//which fetch all names and scores from the database 
+
+getScores().then((data)=>{
+    //todo: next class 
+    console.log(data)
+})
